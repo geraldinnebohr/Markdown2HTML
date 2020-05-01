0dfbd2f77d34dacd9c79bba086e815766885cf2e
geraldinnebohr <geraldinnebohr@gmail.com>
2020-04-30 19:26:50 -0500

    [FIX] sys stderr write

--- markdown2html.py
+++ markdown2html.py
@@ -7,11 +7,10 @@ import os.path
 if __name__ == "__main__":
     lenght = len(sys.argv)
     if lenght < 3:
-        print("Usage: ./markdown2html.py README.md README.html",
-              file=sys.stderr)
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
         exit(1)
     elif os.path.exists(sys.argv[1]) is False:
-        print("Missing " + sys.argv[1], file=sys.stderr)
+        sys.stderr.write("Missing " + sys.argv[1])
         exit(1)
     else:
         exit(0)
