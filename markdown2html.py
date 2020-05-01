c9823a0b19797486a0d3bd99bc7d6b0fa58325df
geraldinnebohr <geraldinnebohr@gmail.com>
2020-04-30 19:28:21 -0500

    [FIX] sys exit

--- markdown2html.py
+++ markdown2html.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     lenght = len(sys.argv)
     if lenght < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
-        exit(1)
+        sys.exit(1)
     elif os.path.exists(sys.argv[1]) is False:
         sys.stderr.write("Missing " + sys.argv[1])
-        exit(1)
+        sys.exit(1)
     else:
         exit(0)
