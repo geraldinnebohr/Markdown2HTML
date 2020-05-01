5ee393e4eea77b034bb77eaef9eb151bf6d53847
geraldinnebohr <geraldinnebohr@gmail.com>
2020-04-30 19:21:28 -0500

    [FIX] sys import

--- markdown2html.py
+++ markdown2html.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """script that takes 2 arguments"""
-from sys import argv, stderr
+import sys
 import os.path
 
 
 if __name__ == "__main__":
-    lenght = len(argv)
+    lenght = len(sys.argv)
     if lenght < 3:
         print("Usage: ./markdown2html.py README.md README.html",
-              file=stderr)
+              file=sys.stderr)
         exit(1)
-    elif os.path.exists(argv[1]) is False:
-        print("Missing " + argv[1], file=stderr)
+    elif os.path.exists(sys.argv[1]) is False:
+        print("Missing " + sys.argv[1], file=sys.stderr)
         exit(1)
     else:
         exit(0)
